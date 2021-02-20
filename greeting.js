ba0f732f565e8c0f552d4746fb7c58f5ab3d308b
larry1121 <78005200+larry1121@users.noreply.github.com>
2021-02-20 17:38:13 +0900

    Update greeting.js

--- greeting.js
+++ greeting.js
@@ -1,56 +1,42 @@
 const form = document.querySelector(".js-form"),
-  input = form.querySelector("input"),
-  greeting = document.querySelector(".js-greetings");
+ input = form.querySelector("input"),
+ greeting = document.querySelector(".js-greetings");
 
-const USER_LS = "currentUser",
+ const USER_LS = "currentUser",
   SHOWING_CN = "showing";
 
-function removeName(event) {
-  if (USER_LS !== null) {
-    greeting.classList.remove(SHOWING_CN);
-    form.classList.add(SHOWING_CN);
-    localStorage.removeItem(USER_LS);
-  }
-}
-
-function saveName(text) {
-  localStorage.setItem(USER_LS, text);
+function saveName(text){
+    localStorage.setItem(USER_LS, text);
 }
 
 function handleSubmit(event) {
-  event.preventDefault();
-  const currentValue = input.value;
-  paintGreeting(currentValue);
-  saveName(currentValue);
-}
-
+    event.preventDefault();
+    const currnentValue = input.value;
+    paintGreeting(currnentValue);
+    saveName(currnentValue); 
+} 
 function askForName() {
-  form.classList.add(SHOWING_CN);
-  form.addEventListener("submit", handleSubmit);
-  
+    form.classList.add(SHOWING_CN);
+    form.addEventListener('submit', handleSubmit);  
 }
 
-function paintGreeting(text) {
-  form.classList.remove(SHOWING_CN);
-  greeting.classList.add(SHOWING_CN);
-  greeting.innerText = `Hello ${text}`;
-}
+ function paintGreeting(text) {
+    form.classList.remove(SHOWING_CN); 
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+ }
+ function loadName() {
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser === null){
+        askForName();
+    } else {
+        paintGreeting(currentUser);
+    }
+ }
 
-function loadName() {
-  const currentUser = localStorage.getItem(USER_LS);
-  
+ function init() {
+    loadName();
+ }
 
-  if (currentUser === null) {
-    askForName();
-  } else {
-    paintGreeting(currentUser);
-    form.classList.remove(SHOWING_CN);
-  }
- 
-}
-
-function init() {
-  loadName();
-}
-
-init();
+ init();
+© 2021 GitHub, Inc.
